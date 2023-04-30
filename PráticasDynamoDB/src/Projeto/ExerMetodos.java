714642df4a985bcee0b61fc716c8697ebb5fae9b
IranSS <iranildosousa1234@gmail.com>
2023-04-30 10:34:54 -0300

    First commit

---
+++ PráticasDynamoDB/src/Projeto/ExerMetodos.java
@@ -0,0 +1,13 @@
+package Projeto;
+
+public class ExerMetodos {
+	public static void main(String[] args) {	
+		
+		Oper.Soma(5, 6);
+		Oper.Subtracao(2, 2);
+		Oper.dividir(4, 2);
+		Oper.multiplicar(2, 5);
+		
+		System.out.println(Oper.Soma(5, 2));
+	}
+}
---
+++ PráticasDynamoDB/src/Projeto/Oper.java
@@ -0,0 +1,29 @@
+package Projeto;
+
+public class Oper {
+
+	public static double Soma(double n1, double n2) {
+		double resultado = n1 + n2;
+		//System.out.println(resultado);
+		
+		return resultado;
+	}
+	public static double Subtracao(double n1, double n2) {
+		double resultado = n1 - n2;
+		//System.out.println(resultado);
+		
+		return resultado;
+	}
+	public static double dividir(double n1, double n2) {
+		double resultado = n1 / n2;
+		//System.out.println(resultado);
+		
+		return resultado;
+	}
+	public static double multiplicar(double n1, double n2) {
+		double resultado = n1 * n2;
+		//System.out.println(resultado);
+		
+		return resultado;
+	}
+}
